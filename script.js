ba360d064e6c321d4d666a5dbf3d3270d1f2e4de
mohil c <mohilc24@gmail.com>
2024-09-19 23:01:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,29 @@
 //active header sticky
-
 const header = document.querySelector("header");
 
-window.addEventListener("scroll", ()=>{
-
-header.classList.toggle("sticky",window.scrollY > 50)
-
-})
+window.addEventListener("scroll", () => {
+    header.classList.toggle("sticky", window.scrollY > 50);
+});
 
 //active scroll menu
-
 const lilink = document.querySelectorAll("header ul li a");
-
 const section = document.querySelectorAll("section");
 
-function activeMenu(){
-
-let secLength = section.length;
-
-while(--secLength && window.screen + 500 < section [secLength].offsetTop){}
-
-lilink.forEach(sec => sec.classList.remove("active"));
-
-liLink [secLength].classList.add("active");
-
+function activeMenu() {
+    let secLength = section.length;
+    while (--secLength && window.scrollY + 500 < section[secLength].offsetTop) {}
+    lilink.forEach(sec => sec.classList.remove("active"));
+    lilink[secLength].classList.add("active");
 }
 
 activeMenu();
-
 window.addEventListener("scroll", activeMenu);
-//active menu icon
-const menuIcon =document.querySelector("#menu-icon");
-const navlist =document.querySelector(".navalist");
 
-menuIcon.onclick =() =>{
+//active menu icon
+const menuIcon = document.querySelector("#menu-icon");
+const navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = () => {
     menuIcon.classList.toggle("bx-x");
-    navlist.classList.toggle("open")
-}
+    navlist.classList.toggle("open");
+};
